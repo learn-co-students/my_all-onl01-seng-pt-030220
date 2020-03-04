b9ee765509ae60c2aa43ae4ed32f6dc486d30da3
German Cruz <cruzgerman216@yahoo.com>
2020-03-04 06:10:11 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  on = true
+  while i < collection.length
+    if yield(collection[i]) == false
+      on = false
+    end
+    i = i + 1
+  end
+  return on
 end
