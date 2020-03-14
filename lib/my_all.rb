dc501a2839ef40dfb8ecac0facd38733e32053ba
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-03-14 01:34:08 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,18 @@
 require 'pry'
 
 def my_all?(collection)
-
+ i = 0
+ block_return_Values = []
+ while i < collection.length
+ block_return_Values << yield(collection[i])
+ i += 1 
+ end
+ 
+ if block_return_Values.include?(false)
+   false
+  else 
+    true 
+  end
 end
+
+   
